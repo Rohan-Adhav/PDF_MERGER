2d95aa806826d6ba52feea522b363622e3639b8f
Rohan Kiran Adhav <155009973+Rohan-Adhav@users.noreply.github.com>
2025-05-01 15:01:06 +0530

    Add files via upload

---
+++ pdf-merger-project/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const path = require("path")
+const multer  = require('multer')
+const {mergePdfs} = require('./javascript/merge.cjs')
+const upload = multer({ dest: 'uploads/' })
+app.use('/static',express.static('public'))
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,"templates/index.html"))
+})
+app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
+    // req.files is array of `photos` files
+    // req.body will contain the text fields, if there were any
+    console.log(req.files)
+    let d = await mergePdfs(path.join(__dirname,req.files[0].path),path.join(__dirname,req.files[1].path))
+    // res.send({data:req.files})
+    res.redirect(`http://localhost:3000/static/${d}.pdf`)
+  })
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
